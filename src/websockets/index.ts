cb5ede6199c091f265607a59247453a0c1c8f066
khanhthanhdev <thanhkt27507@gmail.com>
2025-08-18 09:45:54 +0700

    feat: add WebSocket barrel exports for centralized connection management

---
+++ src/websockets/index.ts
@@ -0,0 +1,4 @@
+// Barrel exports for WebSockets (legacy + simplified)
+export * from './dto';
+export * from './interfaces/centralized-connection.interface';
+export { CentralizedConnectionManagerService } from './services/centralized-connection-manager.service';
